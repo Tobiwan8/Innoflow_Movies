9020fe8a675a5900c225cc4741a9f287163df4f4
Unicow8 <tobiaslarsen93@gmail.com>
2022-05-19 14:18:43 +0200

    forvirringen er total

--- Programmet/Innoflow_Movies/Innoflow_Movies/Program.cs
+++ Programmet/Innoflow_Movies/Innoflow_Movies/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Globalization;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Innoflow_Movies
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
@@ -14,21 +16,61 @@ namespace Innoflow_Movies
             string[] products = File.ReadAllLines(@"C:\Users\toblar\Desktop\MovieData\MovieData\Products.txt");
             string[] currentUser = File.ReadAllLines(@"C:\Users\toblar\Desktop\MovieData\MovieData\CurrentUserSession.txt");
 
+            double rating = 0;
+
+            List<ProductDto> productsList = new List<ProductDto>();
             //Populære film
-            Console.WriteLine(users[0]);
 
-
-            /*foreach (var item in products)
+            double HighstRating = 0;
+            string informationForHighestRatingTitle = string.Empty;
+            string informationForHighestRatingTitle2 = string.Empty;
+            foreach (string product in products)
             {
-                console.writeline("bedst ratede film: " + rating);
-                console.writeline("mest købte: " + købte);
-            }*/
+                string[] productItems = product.Split(',');
 
+                var currentRating = Convert.ToDouble(productItems[8].Replace('.',','));
+
+                if (currentRating > HighstRating)
+                {
+                    HighstRating = currentRating;
+                    informationForHighestRatingTitle2 = informationForHighestRatingTitle;
+                    informationForHighestRatingTitle = product;
+                }
+
+
+                //var template = new ProductDto()
+                //{
+                //    Year = 2022
+                //};
+                //template.Id = Convert.ToInt32(productItems[0]);
+                //template.Title = productItems[1];
+                //template.Year = Convert.ToInt32(productItems[2]);
+
+                //productsList.Add(template);
+
+                //decimal rating = Convert.ToDouble(productItems[8], CultureInfo.InvariantCulture);
+                int price = Convert.ToInt32(productItems[9]);
+
+                //Console.WriteLine(rating);
+                Console.WriteLine(product);
+                //Console.WriteLine("best rated: " + rating);
+                }
+
+            var maxRating = productsList.Max(x => x.Rating);
+
+            var maxRatingProduct = productsList.Max(x => x.Rating);
             //Anbefalede film
 
-
-
             Console.ReadKey();
+        }
+
+        public class ProductDto
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public int Year { get; set; }
+            public string Type { get; set; }
+            public int Rating { get; set; }
         }
     }
 }
