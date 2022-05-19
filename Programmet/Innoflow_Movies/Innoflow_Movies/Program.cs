8ffd29374ef74852889d287c1d4a5f082a39d0e0
Unicow8 <tobiaslarsen93@gmail.com>
2022-05-19 11:42:17 +0200

    first commit2

---
+++ Programmet/Innoflow_Movies/Innoflow_Movies/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Innoflow_Movies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] users = File.ReadAllLines(@"C:\Users\toblar\Desktop\MovieData\MovieData\Users.txt");
+            string[] products = File.ReadAllLines(@"C:\Users\toblar\Desktop\MovieData\MovieData\Products.txt");
+            string[] currentUser = File.ReadAllLines(@"C:\Users\toblar\Desktop\MovieData\MovieData\CurrentUserSession.txt");
+
+            //Populære film
+            Console.WriteLine(users[0]);
+
+
+            /*foreach (var item in products)
+            {
+                console.writeline("bedst ratede film: " + rating);
+                console.writeline("mest købte: " + købte);
+            }*/
+
+            //Anbefalede film
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
